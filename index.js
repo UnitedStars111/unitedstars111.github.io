aee78e06490e8944ab6c2e9d97ea8156cc6a8628
UnitedStars111 <115163796+UnitedStars111@users.noreply.github.com>
2025-04-24 15:28:56 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-console.log(`Hello`);
-console.log(`I like pizza!`);
+/* typewriter title :D */
 
-let x;
-x = 10;
-console.log(`The average age in Youtube comments is $(x)`);
+<script language="javascript">
+var titletext="My personal website!"
+var repeat=true;    // SET TO true TO REPEAT, false TO "TYPE" OUT TITLE ONLY ONCE.
+set=setTimeout;
+var index=0;
+function scrolltitle(){
+if(index<=titletext.length){
+document.title=titletext.substring(0,index);
+index++;
+set('scrolltitle()', 200);
+}else{
+index=0;
+if(repeat)set('scrolltitle()',1000);
+}}
+window.onload=function(){
+if(!document.layers)set('scrolltitle()',1000);
+}
+</script>
