00d33208135e65ba721837a681dde090ad00e3c4
UnitedStars111 <115163796+UnitedStars111@users.noreply.github.com>
2025-05-11 15:06:43 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,14 +4,15 @@ var repeat=true;
 set=setTimeout;
 var index=0;
 function scrolltitle(){
-if(index<=titletext.length){
-document.title=titletext.substring(0,index);
-index++;
-set('scrolltitle()', 200);
-}else{
-index=0;
+  if(index<=titletext.length){
+    document.title=titletext.substring(0,index);
+    index++;
+    set('scrolltitle()', 200);
+  } else {
+    index=0;
 if(repeat)set('scrolltitle()',1000);
-}}
+  }
+}
 window.onload=function(){
 if(!document.layers)set('scrolltitle()',1000);
 }
