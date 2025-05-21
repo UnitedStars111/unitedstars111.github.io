4fc45b5d33157e048198e6b990265fb05725f891
UnitedStars111 <115163796+UnitedStars111@users.noreply.github.com>
2025-05-21 13:42:46 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ var repeat=true;
 set=setTimeout;
 var index=0;
 function scrolltitle(){
-  if(index<=titletext.length){
-    document.title=titletext.substring(0,index);
-    index++;
-    set('scrolltitle()', 200);
-  } else {
-    index=0;
-if(repeat)set('scrolltitle()',1000);
+  if (index<=titletext.length) {
+      document.title=titletext.substring(0,index);
+      index++;
+      set('scrolltitle()', 200);
+    } else {
+      index=0;
+      if(repeat)set('scrolltitle()',1000);
   }
 }
 window.onload=function(){
